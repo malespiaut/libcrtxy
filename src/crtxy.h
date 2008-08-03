e9678975ce61647a93fe0c91d9b80e1b121caaf4
wkendrick <wkendrick>
2008-08-03 06:59:20 +0000

    Added initial comments to public functions/macros in header file.

--- src/crtxy.h
+++ src/crtxy.h
@@ -85,17 +85,17 @@ typedef struct XY_options_s {
 
 /* Error constants: */
 enum {
-  XY_ERR_NONE,
-  XY_ERR_OPTION_BAD,
-  XY_ERR_OPTION_UNKNOWN,
-  XY_ERR_FILE_CANT_OPEN,
-  XY_ERR_MEM_CANT_ALLOC,
-  XY_ERR_INIT_VIDEO,
-  XY_ERR_INIT_DISPLAY,
-  XY_ERR_INIT_UNSUPPORTED_BPP,
-  XY_ERR_BITMAP_CANT_DECODE,
-  XY_ERR_BITMAP_CANT_CONVERT,
-  XY_ERR_BITMAP_CANT_SCALE,
+  XY_ERR_NONE, // No error
+  XY_ERR_OPTION_BAD, // Bad value to an option
+  XY_ERR_OPTION_UNKNOWN, // Unrecognized option
+  XY_ERR_FILE_CANT_OPEN, // Cannot open file
+  XY_ERR_MEM_CANT_ALLOC, // Cannot allocate memory
+  XY_ERR_INIT_VIDEO, // Cannot initialize video subsystem
+  XY_ERR_INIT_DISPLAY, // Cannot open display
+  XY_ERR_INIT_UNSUPPORTED_BPP, // Unsupported (so far as we're concerned) bpp
+  XY_ERR_BITMAP_CANT_DECODE, // Error decoding image file (IMG_Load failed)
+  XY_ERR_BITMAP_CANT_CONVERT, // Cannot convert a surface to the display fmt.
+  XY_ERR_BITMAP_CANT_SCALE, // Cannot scale an image (probably mem. alloc. fail)
   NUM_XY_ERRS
 };
 
@@ -120,81 +120,146 @@ enum {
 
 /* --- Public function prototypes --- */
 
-/* Options: */
+/* - Options: - */
+
+/* Set opts to default (libcrtxy's compiled-time) options */
 void XY_default_options(XY_options * opts);
+
+/* Load global, then local (user) libcrtxy config files into opts */
 XY_bool XY_load_options(XY_options * opts);
+
+/* Load arbitrary config file into opts */
 XY_bool XY_load_options_from_file(char * fname, XY_options * opts,
                                   XY_bool ignore_unknowns);
+
+/* Parse libcrtxy-related arguments into opts */
 int XY_parse_options(int * argc, char * argv[], XY_options * opts);
+
+/* Read any libcrtxy-related environment variables into opts */
 XY_bool XY_parse_envvars(XY_options * opts);
 
-/* Init and quit: */
+
+/* - Init and quit: - */
+
+/* Initialize SDL (video only) and libcrtxy (with rendering and video options
+   from opts), and set the virtual canvas size */
 XY_bool XY_init(XY_options * opts, XY_fixed canvasw, XY_fixed canvash);
+
+/* Shut down libcrtxy and SDL */
 void XY_quit(void);
 
-/* Errors: */
+
+/* - Errors: - */
+
+/* Gets the most recent error code */
+int XY_errcode(void);
+
+/* Gets a string representing the most recent error code */
 const char * XY_errstr(void);
+
+/* Dumps options in opts to the file stream (eg, stderr or an opened logfile) */
 void XY_print_options(FILE * fi, XY_options opts);
 
-/* Load and free bitmap: */
+
+/* - Load and free bitmap: - */
+
+/* Create a bitmap based on an image file */
 XY_bitmap * XY_load_bitmap(char * filename);
+
+/* Create a bitmap based on image data in a buffer */
 XY_bitmap * XY_load_bitmap_from_buffer(unsigned char * buffer, int size);
+
+/* Free a bitmap */
 void XY_free_bitmap(XY_bitmap * bitmap);
 
-/* Setting background: */
 
+/* - Setting background: - */
+
+/* Set the background color, and optional bitmap, its position, and
+   options for scaling it to the screen size.  Enables background bitmap. */
 XY_bool XY_set_background(XY_color color, XY_bitmap * bitmap,
                           XY_fixed x, XY_fixed y, int posflags, int scaling);
+
+/* Enable or disable the background bitmap (affects next frame) */
 void XY_enable_background(XY_bool enable);
 
 
-/* Colors: */
+/* - Colors: - */
 
+/* Combines R, G, B and A values into an XY_color */
 XY_color XY_setcolor(Uint8 r, Uint8 g, Uint8 b, Uint8 a);
+
+/* Breaks an XY_color into its R, G, B and A values */
 void XY_getcolor(XY_color c, Uint8 * r, Uint8 * g, Uint8 * b, Uint8 * a);
 
 
-/* Starting and ending a drawing frame: */
+/* - Starting and ending a drawing frame: - */
 
+/* Mark the start of a frame. Screen backbuffer is wiped to the background
+   color and/or bitmap.  Allows setting of preferred FPS. */
 void XY_start_frame(int fps);
+
+/* Mark the end of a frame. Screen backbuffer is made visible.
+   Optionally, pause until (1000/fps) miliseconds have passed since
+   XY_start_frame() was called. */
 int XY_end_frame(XY_bool throttle);
 
 
-/* Drawing primitives: */
+/* - Drawing primitives: - */
 
+/* Draw a line between (x1,y1) and (x2,y2) (in canvas virtual world units)
+   in the specified color/alpha */
 void XY_draw_line(XY_fixed x1, XY_fixed y1, XY_fixed x2, XY_fixed y2,
                   XY_color color);
+
+/* Draw a point at (x,y) (in canvas virtual world units)
+   in the specified color/alpha */
 void XY_draw_point(XY_fixed x, XY_fixed y, XY_color color);
 
 
-/* Fixed-point math functions: */
+/* - Fixed-point math functions: - */
 
+/* Multiply a * b */
 #define XY_mult(a,b) (((a) >> XY_FIXED_SHIFT_HALF) * \
 		      ((b) >> XY_FIXED_SHIFT_HALF))
+
+/* Divide a / b.  XY_div makes sure b is not 0, returns Not-A-Number if so. */
 #define XY_qdiv(a,b) (((a) / \
 		       ((b) >> XY_FIXED_SHIFT_HALF)) \
                       << XY_FIXED_SHIFT_HALF)
 #define XY_div(a,b) ((b) == 0 ? XY_FIXED_NAN : XY_qdiv((a),(b)))
 
+/* Returns the fractional part of 'a'.  XY_rfpart returns one minus that. */
 #define XY_fpart(a) ((a) & (XY_FIXED_ONE - 1))
-#define XY_ipart(a) ((a) - XY_fpart(a))
 #define XY_rfpart(a) (XY_FIXED_ONE - XY_fpart(a))
+
+/* Returns the integer (whole) part of 'a' */
+#define XY_ipart(a) ((a) - XY_fpart(a))
+
+/* Rounds 'a' up to the nearest integer (whole). */
 #define XY_round(a) (XY_ipart((a) + (1 << (XY_FIXED_SHIFT - 1))))
 
+/* Returns cosin() and sine() of 'degrees'. ('degrees' is a non-fixed-point
+   value, in degrees.  Values outside 0-359 are accounted for. */
 XY_fixed XY_cos(int degrees);
 #define XY_sin(degrees) (XY_cos(90 - (degrees)))
 
 
-/* Screen/canvas conversions and queries: */
+/* - Screen/canvas conversions and queries: - */
 
+/* Convert a screen coordinate (an integer; eg, where the mouse was clicked)
+   into canvas virtual world units (fixed point) */
 XY_fixed XY_screenx_to_canvasx(int sx);
 XY_fixed XY_screeny_to_canvasy(int sy);
 void XY_screen_to_canvas(int sx, int sy, XY_fixed * cx, XY_fixed * cy);
 
+/* Convert a canvas virtual world coordinate (fixed point) into the
+   nearest screen coordinate (an integer) */
 int XY_canvasx_to_screenx(int cx);
 int XY_canvasy_to_screeny(int cy);
 void XY_canvas_to_screen(XY_fixed cx, XY_fixed cy, int * sx, int * sy);
 
+/* Returns the screen's width and height, in pixels (integer) */
 int XY_get_screenw(void);
 int XY_get_screenh(void);
 
